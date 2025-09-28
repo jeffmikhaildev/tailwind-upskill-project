d5864b357eba9789a9304856780c273b96635bd7
jeffmikhail <jeffmikhail.dev@gmail.com>
2025-09-28 15:44:29 +0800

    vercel.json file

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -16,7 +16,11 @@ const SignIn = () => {
 						className="w-full px-4 py-2 border border-neutral-600 rounded-md bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-orange-600"
 					/>
 
-					<button className="w-full py-2 px-4 bg-gradient-to-r from-orange-500 to-orange-800 rounded-md text-white font-semibold transition-all duration-300 hover:scale-105">Sign In</button>
+					<button
+						disabled
+						className="w-full py-2 px-4 bg-gradient-to-r from-orange-500 to-orange-800 rounded-md text-white font-semibold transition-all duration-300 hover:scale-105">
+						Sign In
+					</button>
 				</form>
 
 				<p className="mt-6 text-center text-sm text-neutral-400">
