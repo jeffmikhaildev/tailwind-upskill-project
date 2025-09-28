a4bf66bbf5a8a19b5bbdbed48e27238d768b6bb8
jeffmikhail <jeffmikhail.dev@gmail.com>
2025-09-28 10:09:16 +0800

    hero, features, workflow and testimonials sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import Features from "./components/Features";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Workflow from "./components/Workflow";
 
 function App() {
 	return (
 		<>
 			<Navbar />
+			<div className="max-w-7xl mx-auto pt-20 px-6">
+				<Hero />
+				<Features />
+				<Workflow />
+				<Pricing />
+			</div>
 		</>
 	);
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,3 @@
-import { BotMessageSquare } from "lucide-react";
-import { BatteryCharging } from "lucide-react";
-import { Fingerprint } from "lucide-react";
-import { ShieldHalf } from "lucide-react";
-import { PlugZap } from "lucide-react";
-import { GlobeLock } from "lucide-react";
-
 import user1 from "../assets/profile-pictures/user1.jpg";
 import user2 from "../assets/profile-pictures/user2.jpg";
 import user3 from "../assets/profile-pictures/user3.jpg";
@@ -55,39 +48,6 @@ export const testimonials = [
 		company: "Synergy Systems",
 		image: user6,
 		text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
-	},
-];
-
-export const features = [
-	{
-		icon: BotMessageSquare,
-		text: "Drag-and-Drop Interface",
-		description: "Easily design and arrange your VR environments with a user-friendly drag-and-drop interface.",
-	},
-	{
-		icon: Fingerprint,
-		text: "Multi-Platform Compatibility",
-		description: "Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and VR headsets.",
-	},
-	{
-		icon: ShieldHalf,
-		text: "Built-in Templates",
-		description: "Jumpstart your VR projects with a variety of built-in templates for different types of applications and environments.",
-	},
-	{
-		icon: BatteryCharging,
-		text: "Real-Time Preview",
-		description: "Preview your VR application in real-time as you make changes, allowing for quick iterations and adjustments.",
-	},
-	{
-		icon: PlugZap,
-		text: "Collaboration Tools",
-		description: "Work together with your team in real-time on VR projects, enabling seamless collaboration and idea sharing.",
-	},
-	{
-		icon: GlobeLock,
-		text: "Analytics Dashboard",
-		description: "Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.",
 	},
 ];
 
